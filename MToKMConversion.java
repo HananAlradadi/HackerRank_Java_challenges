ccbde29371190db1cd75887e7ee55e4f98edf833
HananAlradadi <87634898+HananAlradadi@users.noreply.github.com>
2023-06-26 00:35:16 +0300

    Create MToKMConversion.java

---
+++ MToKMConversion.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner input = new Scanner(System.in);
+        double m = input.nextDouble() ;
+        double km = m * 1.6 ;
+        System.out.println("The equivalent distance in kilometers is " + km + " km ") ;
+    }
+}

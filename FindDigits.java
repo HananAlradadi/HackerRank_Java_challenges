447e3d535c69a999f68163029bef6a4a3540b574
HananAlradadi <87634898+HananAlradadi@users.noreply.github.com>
2023-06-26 00:07:17 +0300

    Create FindDigits.java

---
+++ FindDigits.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the findDigits function below.
+    static int findDigits(String n) {
+        int count = 0 ;
+        int s = Integer.parseInt(n);
+       for(int i = 0 ; i < n.length() ; i++) {
+           int z = Integer.parseInt(n.substring(i,i+1));
+        if(z!=0){
+
+      if(s%z==0){
+            count++;
+        }
+       }
+       }
+     return count ;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            String n = scanner.nextLine();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int result = findDigits(n);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}

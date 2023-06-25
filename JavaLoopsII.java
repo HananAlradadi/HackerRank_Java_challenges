05994941b947b3bdd98b76ca7623831f41a7dab0
HananAlradadi <87634898+HananAlradadi@users.noreply.github.com>
2023-06-26 00:46:01 +0300

    Create JavaLoopsII.java

---
+++ JavaLoopsII.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+import java.lang.Math ;
+
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+           
+            for(int x=0 ; x<n ; x++){
+                a += (int)( Math.pow(2,x)*b);
+                 System.out.print(a + " ");
+            }
+           System.out.println();
+        
+             
+    
+        }
+        in.close();
+    }
+}
+
